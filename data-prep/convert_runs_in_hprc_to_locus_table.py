ea72916858bf75ab4d61d1e06925b4ed7c85dde8
bw2 <ben.weisburd@gmail.com>
2025-04-28 11:10:10 -0400

    export biallelic histogram column from HPRC100

--- data-prep/convert_runs_in_hprc_to_locus_table.py
+++ data-prep/convert_runs_in_hprc_to_locus_table.py
@@ -21,14 +21,14 @@ import numpy as np
 import tqdm
 parser = argparse.ArgumentParser()
 parser.add_argument("--input-tsv", type=str, default="runs_in_hprc.2025_04.txt.gz")
-parser.add_argument("--output-path", type=str, default="runs_in_hprc.2025_04.grouped_by_locus_and_motif.tsv.gz")
+parser.add_argument("--output-path", type=str, default="runs_in_hprc.2025_04.grouped_by_locus_and_motif.with_biallelic_histogram.tsv.gz")
 args = parser.parse_args()
 
 # check that file exists
 if not os.path.exists(args.input_tsv):
     parser.error(f"File {args.input_tsv} does not exist")
 
-def process_group(locus_id, motif, allele_sizes, output_file):
+def process_group(locus_id, motif, allele_sizes, alleles_for_current_key_by_sample_id, output_file):
     """Process a group of allele sizes and write statistics to output file."""
     
     if not allele_sizes:
@@ -50,13 +50,24 @@ def process_group(locus_id, motif, allele_sizes, output_file):
     allele_counts = collections.Counter(allele_sizes)
     mode_allele, _ = allele_counts.most_common(1)[0]
 
-    allele_size_histogram = ",".join(f"{allele_size}x:{count}" for allele_size, count in allele_counts.items())
-    
+    genotype_counts = collections.defaultdict(int)
+    for sample_id, allele_list in alleles_for_current_key_by_sample_id.items():
+        if len(allele_list) == 1:
+            allele_list = allele_list * 2
+        elif len(allele_list) != 2:
+            raise ValueError(f"Found {len(allele_list)} alleles for {sample_id} in {locus_id} {motif}")
+
+        genotype_counts[tuple(sorted(allele_list))] += 1
+
+    allele_size_histogram = ",".join(f"{allele_size}x:{count}" for allele_size, count in sorted(allele_counts.items()))
+    biallelic_histogram = ",".join(f"{genotype[0]}/{genotype[1]}:{count}" for genotype, count in sorted(genotype_counts.items(), key=lambda x: (x[0][1], x[0][0])))
+
     # Write to output file
     output_file.write("\t".join(map(str, [
         locus_id, 
         motif, 
         allele_size_histogram, 
+        biallelic_histogram,
         mode_allele, 
         f"{np.mean(allele_sizes):.3f}",
         f"{np.std(allele_sizes):.3f}", 
@@ -75,6 +86,7 @@ def main():
         "locus_id", 
         "motif", 
         "allele_size_histogram", 
+        "biallelic_histogram",
         "mode_allele", 
         "mean",
         "stdev", 
@@ -85,6 +97,8 @@ def main():
     previous_key = None
     previously_seen_keys = set()
     alleles_for_current_key = []
+    alleles_for_current_key_by_sample_id = collections.defaultdict(list)
+
     sample_ids = set()
     counters = collections.Counter()
     for line_number, line in tqdm.tqdm(enumerate(infile, 1), unit=" rows", unit_scale=True, total=896_015_950):
@@ -112,21 +126,23 @@ def main():
             previous_motif = previous_key[1]
             previous_key = current_key
 
-            process_group(previous_locus_id, previous_motif, alleles_for_current_key, outfile)
+            process_group(previous_locus_id, previous_motif, alleles_for_current_key, alleles_for_current_key_by_sample_id, outfile)
             alleles_for_current_key = []
+            alleles_for_current_key_by_sample_id = collections.defaultdict(list)
             counters['output_lines'] += 1
 
         # record the current allele size
         try:
             allele_size = int(allele_size)
             alleles_for_current_key.append(allele_size)
+            alleles_for_current_key_by_sample_id[sample_id].append(allele_size)
         except ValueError:
             print(f"Warning: Skipping invalid allele at line #{line_number}: {current_locus_id} {motif} {sample_id} {allele_size}")
             continue
                 
     # process the last group
     if alleles_for_current_key:
-        process_group(current_locus_id, motif, alleles_for_current_key, outfile)
+        process_group(current_locus_id, motif, alleles_for_current_key, alleles_for_current_key_by_sample_id, outfile)
         counters['output_lines'] += 1
         if current_key in previously_seen_keys:
             parser.error(f"{args.input_tsv} is not sorted by locus id on line #{line_number}: {current_locus_id}") 
